67784f9ea5d87b9d6620de05982ab8227b193b33
Apoorv Gupta <dangenmaster02@gmail.com>
2018-10-06 03:39:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+let app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (e) => {
+        if (e) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+/*app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to the website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
